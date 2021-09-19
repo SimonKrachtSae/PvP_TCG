c4233b1daa2f371a21b8ff9e444799d61587a2d5
DESKTOP-IJ9JA1U\Jakob <jajajakob@freenet.de>
2021-09-19 16:11:07 +0200

    test

--- TcgTest/Assets/Scripts/Board.cs
+++ TcgTest/Assets/Scripts/Board.cs
@@ -9,6 +9,7 @@ using UnityEditor;
 public class Board : MonoBehaviour
 {
     public static Board Instance;
+	int i = 0;
 
     [SerializeField] private List<HandField> playerHandCards;
     public List<HandField> PlayerHandCards { get => playerHandCards; set => playerHandCards = value; }
